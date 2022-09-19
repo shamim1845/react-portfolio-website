9c31ad2d63a16b3478b9929aad85c77d4a520bad
shamim 1845m <samimraj1845@gmail.com>
2022-09-19 09:02:39 +0600

    modified

--- src/components/pages/Blog.jsx
+++ src/components/pages/Blog.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus ullam quaerat dignissimos aperiam accusantium, voluptatibus, omnis, nobis perferendis neque quos quas repellat eos suscipit numquam doloremque rem! Inventore odio expedita ab id ipsam consequuntur ex minima, quis odit unde quae dolorem veniam assumenda eum tempore quasi iusto voluptatum libero distinctio similique cumque placeat dolorum. Enim amet culpa eius illum magnam, voluptatem aut facilis illo dolorem, vel, veritatis distinctio? Eligendi reprehenderit, rerum, maxime, mollitia neque ad dolore exercitationem modi illo rem officia molestiae! Ipsum, minus hic! Harum voluptatem voluptas exercitationem accusantium tempora in ex, explicabo magni ea impedit minima non corporis.</div>
+    <div>lorem100</div>
   )
 }
 
